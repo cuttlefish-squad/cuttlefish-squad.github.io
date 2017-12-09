faba439914f4f9e79b61700a673ffa582f57d9c8
cuttlefish-squad <mfciz99@gmail.com>
2017-12-09 11:16:38 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function login() {
+    var password = document.getElementById('password').value;
+    
+    if (password === 'cookiesforever') {
+        document.getElementById('error').classList.add('hide');
+        window.location.href = 'content.html';
+    } else {
+        document.getElementById('error').classList.remove('hide');
+    }
+}
