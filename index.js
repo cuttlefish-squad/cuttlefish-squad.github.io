913bd6838b179a815f38c721cae749c074256767
cuttlefish-squad <mfciz99@gmail.com>
2017-12-09 11:20:32 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ function login() {
     var password = document.getElementById('password').value;
     
     if (password === 'cookiesforever') {
-        document.getElementById('error').classList.add('hide');
         window.location.href = 'home.html';
-    } else {
-        document.getElementById('error').classList.remove('hide');
-    }
+    } 
 }
